73c91636c10a37e1f6a0836b52c9a62df725f403
isra meryem yılmaz <meryemisraylmz@gmail.com>
2026-05-07 23:29:29 +0300

    Improve retrieval pipeline with metadata and label filtering

--- rag-system/app.py
+++ rag-system/app.py
@@ -11,6 +11,9 @@ with open("data.json", "r", encoding="utf-8") as file:
 
 texts = [item["content"] for item in data]
 ids = [item["id"] for item in data]
+labels = [item["label"] for item in data]
+types = [item["type"] for item in data]
+sources = [item["source"] for item in data]
 
 # model yükle
 model = SentenceTransformer("paraphrase-multilingual-MiniLM-L12-v2")
@@ -25,7 +28,15 @@ collection = client.create_collection(name="dyslexia_data")
 collection.add(
     documents=texts,
     embeddings=embeddings.tolist(),
-    ids=ids
+    ids=ids,
+    metadatas=[
+        {
+            "label": labels[i],
+            "type": types[i],
+            "source": sources[i]
+        }
+        for i in range(len(texts))
+    ]
 )
 
 @app.get("/")
@@ -33,15 +44,34 @@ def home():
     return {"message": "RAG API çalışıyor 🚀"}
 
 @app.get("/search")
-def search(query: str):
+def search(query: str, top_k: int = 3, label: str = None):
     query_embedding = model.encode([query])
 
-    results = collection.query(
+    if label:
+       results = collection.query(
         query_embeddings=query_embedding.tolist(),
-        n_results=3
+        n_results=top_k,
+        where={"label": label}
     )
+    else:
+      results = collection.query(
+        query_embeddings=query_embedding.tolist(),
+        n_results=top_k
+    )
+
+    formatted_results = []
+
+    for i in range(len(results["documents"][0])):
+        formatted_results.append({
+            "content": results["documents"][0][i],
+            "label": results["metadatas"][0][i]["label"],
+            "type": results["metadatas"][0][i]["type"],
+            "source": results["metadatas"][0][i]["source"]
+        })
 
     return {
         "query": query,
-        "results": results["documents"][0]
+        "top_k": top_k,
+        "results": formatted_results
     }
+
